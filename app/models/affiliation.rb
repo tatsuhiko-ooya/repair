15f60e51f8f5618a59471c8ed8d6531426c237b6
tatsuhiko_ooya <tatsuhiko.ooya.prog@gmail.com>
2021-04-13 19:42:23 +0900

    active_hash実装

---
+++ app/models/affiliation.rb
@@ -0,0 +1,13 @@
+class Affiliation < ActiveHash::Base
+  self.data = [
+    { id: 1, name: '--' },
+    { id: 2, name: '製造一課' },
+    { id: 3, name: '製造二課' },
+    { id: 4, name: '製造三課' },
+    { id: 5, name: '製造四課' },
+    { id: 6, name: '機械保全課' }
+  ]
+
+  include ActiveHash::Associations
+  has_many :users
+  end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :affiliation
 
   with_options presence: true do
     validates :name
