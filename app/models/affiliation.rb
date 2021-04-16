82aa3fae0bfdf03648185207c27291ea1e523d4a
tatsuhiko_ooya <tatsuhiko.ooya.prog@gmail.com>
2021-04-16 18:56:17 +0900

    ユーザー新規登録機能の実装

--- app/models/affiliation.rb
+++ app/models/affiliation.rb
@@ -1,6 +1,6 @@
 class Affiliation < ActiveHash::Base
   self.data = [
-    { id: 1, name: '--' },
+    { id: 1, name: '---' },
     { id: 2, name: '製造一課' },
     { id: 3, name: '製造二課' },
     { id: 4, name: '製造三課' },
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,8 +8,14 @@ class User < ApplicationRecord
 
   with_options presence: true do
     validates :name
-    validates :last_name
-    validates :first_name
+    with_options format: { with: /\A[ぁ-んァ-ン一-龥々]/, message: '平仮名、全角カタカナ、漢字のみ使用可能です。' } do
+      validates :last_name
+      validates :first_name
+    end
     validates :affiliation_id, numericality: { other_than: 1 }
   end
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: '英数字を混合させてください' }
+
 end
+
+
